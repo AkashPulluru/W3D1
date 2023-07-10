4e486d13b76bdd1075247555585e5fcea42ac80a
Dan Lee <danielhlee4@gmail.com>
2023-07-10 17:32:39 -0400

    still zipping my_zip

--- practice-for-ch-ruby-enumerables-long-practice-main/enumerables.rb
+++ practice-for-ch-ruby-enumerables-long-practice-main/enumerables.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class Array 
     def my_each(&block)
         i = 0
@@ -54,7 +56,18 @@ class Array
     end 
 
     def my_zip(*arguments)
-        array = Array.new(arguments.length) 
+        array = Array.new(self.length) { Array.new(arguments.length + 1) }
+        array.each_with_index do |sub_array, i|
+            array[i][0] = self[i]
+        end
+        arguments.each_with_index do |arg, i|
+            # debugger
+            if array[i][i + 1] != nil
+                array[i][i + 1] = arg[i]
+            end
+        end
+
+        array
     end 
 
 #     Write `my_zip` to take any number of arguments. It should return a new array
@@ -94,3 +107,13 @@ end
 # p a.my_all? { |num| num < 4 } # => true
 
 # p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
