024dc29635886883b484828c49aa5a1fff8b7b7a
AkashPulluru <akashpulluru@gmail.com>
2023-07-10 16:18:11 -0400

    first changes

--- practice-for-ch-ruby-enumerables-long-practice-main/enumerables.rb
+++ practice-for-ch-ruby-enumerables-long-practice-main/enumerables.rb
@@ -1,5 +1,38 @@
 class Array 
-    def
-
+    def my_each(&block)
+        i = 0
+        while i < self.length 
+            block.call(self[i])
+            i += 1 
+        end 
+        return self 
     end
+
+    def my_select(&block)
+        array = []
+        self.my_each do |ele|
+            if block.call(ele) == true 
+                array << ele 
+            end 
+        end 
+        return array 
+    end 
 end 
+
+# return_value = [1, 2, 3].my_each do |num|
+#     puts num
+#    end.my_each do |num|
+#     puts num
+#    end
+#    # => 1
+#    #    2
+#    #    3
+#    #    1
+#    #    2
+#    #    3
+   
+#    p return_value  # => [1, 2, 3]
+
+# a = [1, 2, 3]
+# p a.my_select { |num| num > 1 } # => [2, 3]
+# p a.my_select { |num| num == 4 } # => []
